6537a14e83b59930310bf9c2c4ad5cbc819c4a01
Saher AbdElaziz <saherabdelaziz@yahoo.com>
2018-08-06 11:53:04 +0200

    add firebase and bootsrtap

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyAlvS7LpNcSYcgyAWA1SJHSEc3-o9i-DTU",
+    authDomain: "oshop-23707.firebaseapp.com",
+    databaseURL: "https://oshop-23707.firebaseio.com",
+    projectId: "oshop-23707",
+    storageBucket: "oshop-23707.appspot.com",
+    messagingSenderId: "1012825762675"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyAlvS7LpNcSYcgyAWA1SJHSEc3-o9i-DTU",
+    authDomain: "oshop-23707.firebaseapp.com",
+    databaseURL: "https://oshop-23707.firebaseio.com",
+    projectId: "oshop-23707",
+    storageBucket: "oshop-23707.appspot.com",
+    messagingSenderId: "1012825762675"
+  }
 };
 
 /*
