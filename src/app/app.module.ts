1e99c5c7d9168ed77a2a21150285d58cecc2d451
Saher AbdElaziz <saherabdelaziz@yahoo.com>
2018-08-14 23:49:53 +0200

    add auth service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './auth.service';
 
 import { environment } from './../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
@@ -53,7 +54,7 @@ import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.componen
      { path:'admin/orders' , component:AdminOrdersComponent},
     ])
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,19 +1,16 @@
-import { AngularFireAuth } from 'angularfire2/auth';
+import { AuthService } from './../auth.service';
 import { Component, OnInit } from '@angular/core';
-import * as firebase from 'firebase';
 @Component({
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
 export class BsNavbarComponent {
-  user: firebase.User;
-  constructor(private afAuth:AngularFireAuth) {
-    afAuth.authState.subscribe(x=> this.user=x);
-   }
+  constructor(public auth:AuthService) {
+     }
 
   logout(){
-    this.afAuth.auth.signOut();
+    this.auth.logout();
   }
 
 }
